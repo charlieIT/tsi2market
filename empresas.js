69115e1571a2bfbc34c29d64745d39f2089ea609
charlieIT <cljljlj@gmail.com>
2017-03-13 17:28:23 +0000

    add empresas.json & empresas.js

---
+++ empresas.js
@@ -0,0 +1,65 @@
+/**
+ * Created by luis on 3/13/17.
+ */
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open("GET", "agenda/empresas.json", true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function init() {
+    loadJSON(function (response) {
+        var actual_JSON = JSON.parse(response);
+
+        var emp_div = $("#empresas-js");
+
+        var grid = document.createElement("div");
+        grid.className = "ui stackable centered grid";
+
+        var row = document.createElement("div");
+        row.className =  "four column row";
+
+        var col_count = 0;
+
+        for (var item in actual_JSON) {
+
+
+
+            for (var emp in actual_JSON[item]) {
+
+                if(col_count == 3) {
+                    var row = document.createElement("div");
+                    row.className =  "four column row";
+                    col_count = 0;
+                }
+
+                var obj = actual_JSON[item][emp];
+                var logo = obj.img;
+
+                var col = document.createElement("column");
+                col.className = "column img-container "+ col_count;
+
+                var img = document.createElement("img");
+                img.src = logo;
+
+                col.appendChild(img);
+                row.appendChild(col);
+
+
+
+                grid.appendChild(row);
+                col_count++;
+            }
+
+        }
+        emp_div.append(grid);
+    });
+}init();
